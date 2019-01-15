4b26160547a125ff0c58d6af89238f7fe2bf7f1c
liulei <liu.lei23@zte.com.cn>
2019-01-15 22:54:31 +0800

    add language feature practice

---
+++ data_analyse/language_feature/python/List/list.py
@@ -0,0 +1,12 @@
+
+def bubble_sort(lst):
+    for passnum in range(len(lst) - 1, 0, -1):
+        for i in range(passnum):
+            if lst[i] > lst[i + 1]:
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp
+
+lst = [54,26,93,17,77,31,44,55,20]
+bubble_sort(lst)
+print("sorted %s" %lst)
