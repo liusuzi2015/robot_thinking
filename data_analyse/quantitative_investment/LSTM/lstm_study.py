18f2c2bf7a5bc13577e6a865aa8298cee17f8a13
liulei <liusuzi2015@aliyun.com>
2019-02-17 09:34:08 +0800

    add LSTM test

---
+++ data_analyse/quantitative_investment/LSTM/lstm_study.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import LSTM,Dense
+import matplotlib.pyplot as plt
+
+# import matplotlib
+# matplotlib.use('TkAgg')
+# import matplotlib.pyplot as plt
+
+from keras.models import load_model
+
+
+
+
+
+
+
+
+
